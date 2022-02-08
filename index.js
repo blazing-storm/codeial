a5c9f6544cbdbfa4ae84c5ad780b9db5eb4594ca
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-02-08 20:50:41 +0530

    Added a layout for our views

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+// add this before router
+app.use(expressLayouts);
 
 // use express router
 app.use('/', require('./routes'));
