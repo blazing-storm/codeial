b5a754000d74fa6be4aa24be9db2da7552b6975d
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-02-08 19:51:27 +0530

    added a router and controller for users

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further router, access from here
+// router.use('/routerName', require('./routerfile));
 
 module.exports = router;
