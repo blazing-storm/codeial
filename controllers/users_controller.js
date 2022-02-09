5448509158f18c92a5853a19e2e3c8a83b6f79cb
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-02-09 16:35:36 +0530

    Created the sign in/up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,27 @@ module.exports.profile = function (req, res) {
         title: "User"
     });
 }
+
+// render the sign up page
+module.exports.signUp = function (req, res) {
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    });
+}
+
+// render the sign in page
+module.exports.signIn = function (req, res) {
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    });
+}
+
+// get the sign up data
+module.exports.create = function(req, res) {
+    // TODO Later
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res) {
+    // TODO Later
+}
