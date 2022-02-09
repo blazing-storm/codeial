a52dc40833d1c0c20404edf04dfd186ab7f3747e
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-02-10 01:42:17 +0530

    Added cookie parser

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,10 @@
 // module.exports.actionName = function(req, res) {}
 
 module.exports.home = function(req, res) {
+    // playing with cookies
+    console.log(req.cookies);
+    res.cookie('user_id', 25);
+    
     return res.render('home', {
         title: "Home"
     });
