b840e81a923187fe38f4a43e8d5476457b9fc526
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-03-01 23:19:33 +0530

    Sign Up completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function (req, res) {
     return res.render('user_profile', {
         title: "User"
@@ -20,7 +22,25 @@ module.exports.signIn = function (req, res) {
 
 // get the sign up data
 module.exports.create = function(req, res) {
-    // TODO Later
+    if(req.body.password != req.body.confirm_password)
+        return res.redirect('back');
+
+    User.findOne({email: req.body.email}, function(err, user) {
+        if(err) { console.log('Error in finding user during sign up'); return; }
+
+        if(!user) {
+            User.create(req.body, function(err, user) {
+                if(err) { console.log('Error in creating user during sign up'); return; }
+
+                console.log(user);
+                return res.redirect('/users/sign-in');
+            });
+        }
+        else {
+            console.log('User already exists!');
+            return res.redirect('back');
+        }
+    });
 }
 
 // sign in and create a session for the user
