8cd65436abbd434b4769cc9b1cbf046fa64927a1
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-03-02 02:17:44 +0530

    Started with Passport Local Authentication

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
 
 const db = require('./config/mongoose');
 
@@ -18,13 +22,27 @@ app.use(express.static('./assets'));
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes'));
-
 // set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 // app.set('views', path.join(__dirname, 'views'));
+
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err) {
     if(err) {
