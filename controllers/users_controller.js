e5876abb71e2d43d86e30d7b42305f6e4ab87d9c
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-03-02 16:53:43 +0530

    Created sign-out and added links to the header. Completed Authentication using Passport JS

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -53,5 +53,12 @@ module.exports.create = function(req, res) {
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res) {
-    return res.redirect('/users/profile');
+    return res.redirect('/');
+}
+
+// destroy the session and sign out the user
+module.exports.destroySession = function(req, res) {
+    req.logout();
+
+    return res.redirect('/');
 }
