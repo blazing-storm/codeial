cb97a3e179323ca7b7a6d949c5f8fca5d82d3da4
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-03-03 01:01:20 +0530

    Nested Population :: Display Comments with Posts

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const homeController = require('../controllers/home_controller');
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 
 // for any further router, access from here
 // router.use('/routerName', require('./routerfile));
