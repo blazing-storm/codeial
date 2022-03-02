9e5bce44e626c975102ca7d5e54df70302f6d02a
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-03-02 14:09:07 +0530

    Error while setting up connect-mongo (v3 vs v4)

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
-const db = require('./config/mongoose');
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -27,6 +27,8 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 // app.set('views', path.join(__dirname, 'views'));
 
+// mongo store is used to store the session cookie in the db
+// connect-mongo is not working because this syntax is outdated (v3 vs v4)
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -35,7 +37,16 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err) {
+            console.log(err || 'connect-mongodb setup is working');
+        }
+    )
 }));
 
 app.use(passport.initialize());
