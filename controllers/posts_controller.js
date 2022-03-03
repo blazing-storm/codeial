1b1354d8378c84f106157a51f78b0c1a8fb5aabc
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-03-04 00:59:09 +0530

    Used Noty JS for animated flash messages

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -6,8 +6,13 @@ module.exports.create = function(req, res) {
         content: req.body.content,
         user: req.user._id
     }, function(err, post) {
-        if(err) { console.log('Error in creating a post'); return; }
+        if(err) {
+            // console.log('Error in creating a post');
+            req.flash('error', err);
+            return res.redirect('back');
+        }
 
+        req.flash('success', 'Post published!');
         return res.redirect('back');
     });
 }
@@ -22,15 +27,19 @@ module.exports.destroy = async function(req, res) {
 
             await Comment.deleteMany({post: req.params.id});
 
+            req.flash('success', 'Post and associated comments deleted!');
+
             return res.redirect('back');
         }
         else {
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
     }
     catch(err) {
-        console.log('Error: ', err);
-        return;
+        // console.log('Error: ', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
