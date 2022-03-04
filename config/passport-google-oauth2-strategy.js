1c5e945af6eb7fdc46100fb75334eead900d1483
Blazing-Storm <mayankgoyalstar@gmail.com>
2022-03-04 23:50:47 +0530

    Completed Social Authentication and logged in with a Google account

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -14,6 +14,7 @@ passport.use(new googleStrategy({
         User.findOne({email: profile.emails[0].value}).exec(function(err, user) {
             if(err) { console.log('Error in passport-google-oauth: ', err); return; }
 
+            console.log(accessToken, refreshToken);
             console.log(profile);
 
             if(user) {
