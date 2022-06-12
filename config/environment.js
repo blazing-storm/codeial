bd621a2ed092393c1ae3d26f538211344190737b
blazing-storm <mayankagrawal.main@gmail.com>
2022-06-12 20:39:32 +0545

    Gulp to minimize the css, js and images

--- config/environment.js
+++ config/environment.js
@@ -60,4 +60,5 @@ const production = {
     }
 }
 
+// module.exports = development;
 module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
